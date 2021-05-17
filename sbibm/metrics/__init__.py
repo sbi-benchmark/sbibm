34c71481922c84608a377a16237f1f6d57c74830
janfb <jan.boelts@tum.de>
2021-05-17 17:40:15 +0200

    add moments metrics.

--- sbibm/metrics/__init__.py
+++ sbibm/metrics/__init__.py
@@ -2,3 +2,5 @@ from sbibm.metrics.c2st import c2st  # noqa: F401
 from sbibm.metrics.ksd import ksd  # noqa: F401
 from sbibm.metrics.mmd import mmd  # noqa: F401
 from sbibm.metrics.ppc import median_distance  # noqa: F401
+from sbibm.metrics.moments import mean_error  # noqa: F401
+from sbibm.metrics.moments import variance_error  # noqa: F401
