0bb9ff12f06e0d84477bdc79843f4248ebfa59ed
Jan <janfb@users.noreply.github.com>
2021-11-09 10:13:29 +0100

    Add mean and variance metrics (#11)

--- sbibm/metrics/__init__.py
+++ sbibm/metrics/__init__.py
@@ -1,4 +1,5 @@
 from sbibm.metrics.c2st import c2st  # noqa: F401
 from sbibm.metrics.ksd import ksd  # noqa: F401
 from sbibm.metrics.mmd import mmd  # noqa: F401
+from sbibm.metrics.moments import posterior_mean_error, posterior_variance_ratio
 from sbibm.metrics.ppc import median_distance  # noqa: F401
