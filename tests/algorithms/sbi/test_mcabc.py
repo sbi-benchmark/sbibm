0dee29f85025889f6dab90dd3266242db0ec1372
Peter Steinbach <p.steinbach@hzdr.de>
2021-11-11 13:04:57 +0100

    basic structure for interface test

---
+++ tests/algorithms/sbi/test_mcabc.py
@@ -0,0 +1,32 @@
+import pytest
+import torch
+
+import sbibm
+from sbibm.algorithms.sbi.mcabc import run as run_posterior
+from sbibm.metrics.c2st import c2st
+
+
+@pytest.mark.parametrize(
+    "task_name",
+    [
+        task_name
+        for task_name in [
+            "gaussian_linear",
+            "two_moons",
+        ]
+    ],
+)
+def test_run_posterior_interface(
+    task_name,
+    num_simulations=1_000,
+    num_samples=50,
+):
+    task = sbibm.get_task(task_name)
+
+    samples = run_posterior(
+        task=task,
+        num_simulations=num_simulations,
+        num_observation=3,
+        num_samples=num_samples,
+    )
+    assert len(samples.shape) > 0
