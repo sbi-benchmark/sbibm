08f8324b26743b8262a4112c51f17064e3f3d3ff
Peter Steinbach <p.steinbach@hzdr.de>
2021-11-12 14:06:57 +0100

    merge abc interface tests

--- tests/algorithms/sbi/test_abc_run_methods.py
+++ tests/algorithms/sbi/test_abc_run_methods.py
@@ -2,28 +2,25 @@ import pytest
 import torch
 
 import sbibm
-from sbibm.algorithms.sbi.mcabc import run as run_posterior
+from sbibm.algorithms.sbi.mcabc import run as run_mcabc
+from sbibm.algorithms.sbi.smcabc import run as run_smcabc
 from sbibm.metrics.c2st import c2st
 
 
 @pytest.mark.parametrize(
     "task_name",
-    [
-        task_name
-        for task_name in [
-            "gaussian_linear",
-            "two_moons",
-        ]
-    ],
+    [tn for tn in ["gaussian_linear", "two_moons"]],
 )
+@pytest.mark.parametrize("run_method", (run_mcabc, run_smcabc))
 def test_run_posterior_interface(
     task_name,
+    run_method,
     num_simulations=100,
     num_samples=5,
 ):
     task = sbibm.get_task(task_name)
 
-    samples, _, _ = run_posterior(
+    samples, _, _ = run_method(
         task=task,
         num_simulations=num_simulations,
         num_observation=3,
