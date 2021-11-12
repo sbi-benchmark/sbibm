26d92f55012f28fbe086807c771ed1a46df56141
Peter Steinbach <p.steinbach@hzdr.de>
2021-11-12 10:50:55 +0100

    removed superfluous code
    - include noref tasks as we don't use the reference posterior
    - add TODO for later

--- tests/tasks/test_task_benchmarks.py
+++ tests/tasks/test_task_benchmarks.py
@@ -17,13 +17,13 @@ torch.manual_seed(47)
 
 @pytest.mark.parametrize(
     "task_name",
-    [tn for tn in get_available_tasks() if not re.search("noref|lotka|sir", tn)],
+    [tn for tn in get_available_tasks() if not re.search("lotka|sir", tn)],
 )
 def test_benchmark_metrics_selfobserved(task_name):
 
     task = get_task(task_name)
 
-    nobs = 1
+    nobs = 1  # maybe randomly dice this?
     theta_o = task.get_prior()(num_samples=nobs)
     sim = task.get_simulator()
     x_o = sim(theta_o)
@@ -45,31 +45,3 @@ def test_benchmark_metrics_selfobserved(task_name):
     value = median_distance(predictive_samples, x_o)
 
     assert value > 0
-
-
-# def test_benchmark_metrics():
-
-#     from sbibm.algorithms.sbi.snpe import run
-#     from sbibm.metrics.ppc import median_distance
-
-#     task = get_task("two_moons")
-#     sim = task.get_simulator()
-
-#     outputs, nsim, logprob_truep = run(
-#         task,
-#         num_observation=7,
-#         num_samples=64,
-#         num_simulations=100,
-#         neural_net="mdn",
-#         num_rounds=1,  # let's do NPE not SNPE (to avoid MCMC)
-#     )
-
-#     assert outputs.shape
-#     assert outputs.shape[0] > 0
-#     assert logprob_truep == None
-
-#     predictive_samples = sim(outputs)
-#     x_o = task.get_observation(7)
-#     value = median_distance(predictive_samples, x_o)
-
-#     assert value > 0
