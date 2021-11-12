c91a8e2374dd77ac7cbf4f387af5453821eee9fe
Peter Steinbach <p.steinbach@hzdr.de>
2021-11-12 17:55:05 +0100

    separation of internal and interface tests

---
+++ tests/tasks/noref_beam/test_interface_noref_beam.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pytest
+import torch
+from pyro import distributions as pdist
+from pyro import util as putil
+
+import sbibm
+from sbibm.algorithms import rej_abc
+from sbibm.algorithms.sbi.snpe import run
+from sbibm.metrics import c2st
+from sbibm.metrics.ppc import median_distance
+
+putil.set_rng_seed(47)
+
+
+def test_no_reference_posterior():
+
+    task = sbibm.get_task("noref_beam")
+
+    with pytest.raises(FileNotFoundError):
+        reference_samples = task.get_reference_posterior_samples(num_observation=1)
+
+
+################################################
+## sbibm compliant API tests as documented in
+## the top-level README.md of sbibm
+
+
+def test_quick_demo_rej_abc():
+
+    task = sbibm.get_task("noref_beam")
+    posterior_samples, _, _ = rej_abc(
+        task=task, num_samples=50, num_observation=1, num_simulations=500
+    )
+
+    assert posterior_samples != None
+
+
+def test_quick_demo_c2st():
+
+    task = sbibm.get_task("noref_beam")
+    posterior_samples, _, _ = rej_abc(
+        task=task, num_samples=50, num_observation=1, num_simulations=500
+    )
+
+    # TODO: catch the error as we don't have a reference posterior
+    reference_samples = task.get_reference_posterior_samples(num_observation=1)
+    c2st_accuracy = c2st(reference_samples, posterior_samples)
+
+    assert c2st_accuracy > 0.0
+    assert c2st_accuracy < 1.0
+
+
+def test_benchmark_metrics_selfobserved():
+
+    task = sbibm.get_task("noref_beam")
+
+    nobs = 1
+    theta_o = task.get_prior()(num_samples=nobs)
+    sim = task.get_simulator()
+    x_o = sim(theta_o)
+
+    outputs, nsim, logprob_truep = run(
+        task,
+        observation=x_o,
+        num_samples=16,
+        num_simulations=64,
+        neural_net="mdn",
+        hidden_features=4,
+        simulation_batch_size=32,
+        training_batch_size=32,
+        num_rounds=1,  # let's do NPE not SNPE (to avoid MCMC)
+    )
+
+    assert outputs.shape
+    assert outputs.shape[0] > 0
+    assert logprob_truep == None
+
+    predictive_samples = sim(outputs)
+    value = median_distance(predictive_samples, x_o)
+
+    assert value > 0
--- tests/tasks/noref_beam/test_internal_noref_beam.py
+++ tests/tasks/noref_beam/test_internal_noref_beam.py
@@ -5,6 +5,10 @@ from pyro import distributions as pdist
 from pyro import util as putil
 
 import sbibm
+from sbibm.algorithms import rej_abc
+from sbibm.algorithms.sbi.snpe import run
+from sbibm.metrics import c2st
+from sbibm.metrics.ppc import median_distance
 from sbibm.tasks.noref_beam.task import (
     NorefBeam,
     bcast_coordinate_field,
@@ -14,8 +18,9 @@ from sbibm.tasks.noref_beam.task import (
 
 putil.set_rng_seed(47)
 
-########### sbibm related ################
-## testing the actual task
+
+################################################
+## API tests that related the internal task code
 
 
 def test_task_constructs():
@@ -23,169 +28,6 @@ def test_task_constructs():
     t = NorefBeam()
 
     assert t
-
-
-def test_obtain_task():
-
-    task = sbibm.get_task("noref_beam")
-
-    assert task is not None
-
-
-def test_obtain_task_modified():
-
-    task = sbibm.get_task("noref_beam", min_axis=0, max_axis=20, flood_samples=64)
-
-    assert task is not None
-    assert task.flood_samples == 64
-    assert task.min_axis == 0
-    assert task.max_axis == 20
-
-
-def test_obtain_prior():
-
-    task = sbibm.get_task("noref_beam")  # See sbibm.get_available_tasks() for all tasks
-    prior = task.get_prior()
-
-    assert prior is not None
-
-
-def test_obtain_simulator():
-
-    task = sbibm.get_task("noref_beam")
-
-    simulator = task.get_simulator()
-
-    assert simulator is not None
-
-
-def test_observe_once():
-
-    task = sbibm.get_task("noref_beam")
-
-    x_o = task.get_observation(num_observation=1)
-
-    assert x_o is not None
-    assert hasattr(x_o, "shape")
-
-
-def test_obtain_prior_samples():
-
-    task = sbibm.get_task("noref_beam")
-    prior = task.get_prior()
-    nsamples = 10
-
-    thetas = prior(num_samples=nsamples)
-
-    assert thetas.shape == (nsamples, 4)
-
-
-def test_simulate_from_thetas():
-
-    task = sbibm.get_task("noref_beam")
-    prior = task.get_prior()
-    sim = task.get_simulator()
-    nsamples = 10
-
-    thetas = prior(num_samples=nsamples)
-    xs = sim(thetas)
-
-    assert xs.shape == (nsamples, 400)
-
-
-def test_no_reference_posterior():
-
-    task = sbibm.get_task("noref_beam")
-
-    with pytest.raises(FileNotFoundError):
-        reference_samples = task.get_reference_posterior_samples(num_observation=1)
-
-
-################################################
-## sbibm compliant API tests as documented in
-## the top-level README.md of sbibm
-
-# @pytest.fixture
-# def vanilla_samples():
-
-#     task = sbibm.get_task("noref_beam")
-#     prior = task.get_prior()
-#     sim = task.get_simulator()
-#     nsamples = 10
-
-#     thetas = prior(num_samples=nsamples)
-#     xs = sim(thetas)
-
-#     return task, thetas, xs
-
-
-def test_quick_demo_rej_abc():
-
-    from sbibm.algorithms import rej_abc
-
-    task = sbibm.get_task("noref_beam")
-    posterior_samples, _, _ = rej_abc(
-        task=task, num_samples=50, num_observation=1, num_simulations=500
-    )
-
-    assert posterior_samples != None
-
-
-def test_quick_demo_c2st():
-
-    from sbibm.algorithms import rej_abc
-
-    task = sbibm.get_task("noref_beam")
-    posterior_samples, _, _ = rej_abc(
-        task=task, num_samples=50, num_observation=1, num_simulations=500
-    )
-
-    from sbibm.metrics import c2st
-
-    # TODO: catch the error as we don't have a reference posterior
-    reference_samples = task.get_reference_posterior_samples(num_observation=1)
-    c2st_accuracy = c2st(reference_samples, posterior_samples)
-
-    assert c2st_accuracy > 0.0
-    assert c2st_accuracy < 1.0
-
-
-def test_benchmark_metrics_selfobserved():
-
-    from sbibm.algorithms.sbi.snpe import run
-    from sbibm.metrics.ppc import median_distance
-
-    task = sbibm.get_task("noref_beam")
-
-    nobs = 1
-    theta_o = task.get_prior()(num_samples=nobs)
-    sim = task.get_simulator()
-    x_o = sim(theta_o)
-
-    outputs, nsim, logprob_truep = run(
-        task,
-        observation=x_o,
-        num_samples=16,
-        num_simulations=64,
-        neural_net="mdn",
-        hidden_features=4,
-        simulation_batch_size=32,
-        training_batch_size=32,
-        num_rounds=1,  # let's do NPE not SNPE (to avoid MCMC)
-    )
-
-    assert outputs.shape
-    assert outputs.shape[0] > 0
-    assert logprob_truep == None
-
-    predictive_samples = sim(outputs)
-    value = median_distance(predictive_samples, x_o)
-
-    assert value > 0
-
-
-################################################
-## API tests that related the internal task code
 
 
 def test_multivariate_normal_constructs():
