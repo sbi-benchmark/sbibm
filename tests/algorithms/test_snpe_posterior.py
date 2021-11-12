641c0d53fa2374f3a080557d9815aa71c8b532fa
Peter Steinbach <p.steinbach@hzdr.de>
2021-11-12 11:36:33 +0100

    check for the shape and roughly for accuracy

--- tests/algorithms/test_snpe_posterior.py
+++ tests/algorithms/test_snpe_posterior.py
@@ -1,38 +1,47 @@
 import pytest
 import torch
-import random
 
 import sbibm
 from sbibm.algorithms.sbi.snpe import run as run_posterior
 from sbibm.metrics.c2st import c2st
 
-#a fast test
+
+# a fast test
 @pytest.mark.parametrize(
     "task_name,num_observation",
     [
         (task_name, num_observation)
-        for task_name in ["gaussian_linear", "slcp",]
-        for num_observation in random.sample(range(1, 11), 2)
+        for task_name in [
+            "gaussian_linear",
+            "gaussian_linear_uniform",
+            "slcp",
+        ]
+        for num_observation in [1, 3]
     ],
 )
 def test_npe_posterior(
-    task_name, num_observation, num_simulations=1_000, num_samples=100
+    task_name, num_observation, num_simulations=2_500, num_samples=50
 ):
     task = sbibm.get_task(task_name)
 
-    samples = run_posterior(
+    predicted, _, _ = run_posterior(
         task=task,
         num_observation=num_observation,
         num_simulations=num_simulations,
         num_samples=num_samples,
         num_rounds=1,
-        neural_net="mdn" #fast test
+        neural_net="mdn",
     )
 
     reference_samples = task.get_reference_posterior_samples(
         num_observation=num_observation
     )
 
-    acc = c2st(samples, reference_samples[:num_samples, :])
+    expected = reference_samples[:num_samples, :]
 
-    assert torch.abs(acc - 0.5) < 0.025
+    assert expected.shape == predicted.shape
+
+    acc = c2st(predicted, expected)
+
+    assert acc > 0.5
+    assert acc < 1.0
