070915688e033a43fdd39b6863e83d21bdccd0ce
jan-matthis <mail@jan-matthis.de>
2021-11-16 13:55:37 +0100

    Disable `sbi` transforms (#36)

---
+++ tests/algorithms/sbi/test_snre_posterior.py
@@ -0,0 +1,47 @@
+import pytest
+import torch
+
+import sbibm
+from sbibm.algorithms.sbi.snre import run as run_posterior
+from sbibm.metrics.c2st import c2st
+
+
+# a fast test
+@pytest.mark.parametrize(
+    "task_name,num_observation",
+    [
+        (task_name, num_observation)
+        for task_name in [
+            "gaussian_linear",
+            "gaussian_linear_uniform",
+        ]
+        for num_observation in [1, 3]
+    ],
+)
+def test_nre_posterior(
+    task_name, num_observation, num_simulations=2_000, num_samples=100
+):
+    task = sbibm.get_task(task_name)
+
+    predicted, _, _ = run_posterior(
+        task=task,
+        num_observation=num_observation,
+        num_simulations=num_simulations,
+        num_samples=num_samples,
+        num_rounds=1,
+        max_num_epochs=30,
+    )
+
+    reference_samples = task.get_reference_posterior_samples(
+        num_observation=num_observation
+    )
+
+    expected = reference_samples[:num_samples, :]
+
+    assert expected.shape == predicted.shape
+
+    acc = c2st(predicted, expected)
+
+    assert acc > 0.5
+    assert acc < 1.0
+    assert acc > 0.6
