94e3aeb7104b820544e5e2f8ccf5cac903b38adf
janfb <jan.boelts@tum.de>
2023-02-21 14:33:20 +0100

    add mnle run fun.

---
+++ sbibm/algorithms/sbi/mnle.py
@@ -0,0 +1,160 @@
+import logging
+import math
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+from sbi import inference as inference
+from sbi.utils.get_nn_models import likelihood_nn
+
+from sbibm.algorithms.sbi.utils import (
+    wrap_posterior,
+    wrap_prior_dist,
+    wrap_simulator_fn,
+)
+from sbibm.tasks.task import Task
+
+from sbibm.tasks.ddm.task import DDM
+from sbibm.tasks.ddm.utils import map_x_to_two_D
+
+
+def run(
+    task: Task,
+    num_samples: int,
+    num_simulations: int,
+    num_observation: Optional[int] = None,
+    observation: Optional[torch.Tensor] = None,
+    num_rounds: int = 1,
+    neural_net: str = "mnle",
+    hidden_features: int = 30,
+    simulation_batch_size: int = 1000,
+    training_batch_size: int = 10000,
+    automatic_transforms_enabled: bool = True,
+    mcmc_method: str = "slice_np_vectorized",
+    mcmc_parameters: Dict[str, Any] = {
+        "num_chains": 100,
+        "thin": 10,
+        "warmup_steps": 25,
+        "init_strategy": "sir",
+        # NOTE: sir kwargs changed: num_candidate_samples = num_batches * batch_size
+        "init_strategy_parameters": {
+            "num_candidate_samples": 10000,
+        },
+    },
+    z_score_x: str = "independent",
+    z_score_theta: str = "independent",
+    mnle_kwargs: Optional[Dict[str, Any]] = None,
+    max_num_epochs: Optional[int] = 2**31 - 1,
+) -> Tuple[torch.Tensor, int, Optional[torch.Tensor]]:
+    """Runs MNLE from `sbi`
+    Args:
+        task: Task instance
+        num_observation: Observation number to load, alternative to `observation`
+        observation: Observation, alternative to `num_observation`
+        num_samples: Number of samples to generate from posterior
+        num_simulations: Simulation budget
+        num_rounds: Number of rounds
+        neural_net: Neural network to use, one of maf / mdn / made / nsf
+        hidden_features: Number of hidden features in network
+        simulation_batch_size: Batch size for simulator
+        training_batch_size: Batch size for training network
+        automatic_transforms_enabled: Whether to enable automatic transforms
+        mcmc_method: MCMC method
+        mcmc_parameters: MCMC parameters
+        z_score_x: Whether to z-score x
+        z_score_theta: Whether to z-score theta
+        max_num_epochs: Maximum number of epochs
+    Returns:
+        Samples from posterior, number of simulator calls, log probability of true params if computable
+    """
+    assert not (num_observation is None and observation is None)
+    assert not (num_observation is not None and observation is not None)
+
+    log = logging.getLogger(__name__)
+
+    if num_rounds == 1:
+        log.info(f"Running MNLE")
+        num_simulations_per_round = num_simulations
+    else:
+        log.info(f"Running SMNLE")
+        num_simulations_per_round = math.floor(num_simulations / num_rounds)
+
+    if simulation_batch_size > num_simulations_per_round:
+        simulation_batch_size = num_simulations_per_round
+        log.warn("Reduced simulation_batch_size to num_simulation_per_round")
+
+    if training_batch_size > num_simulations_per_round:
+        training_batch_size = num_simulations_per_round
+        log.warn("Reduced training_batch_size to num_simulation_per_round")
+
+    prior = task.get_prior_dist()
+    if observation is None:
+        observation = task.get_observation(num_observation)
+
+    simulator = task.get_simulator(max_calls=num_simulations)
+
+    transforms = task._get_transforms(automatic_transforms_enabled)["parameters"]
+    if automatic_transforms_enabled:
+        prior = wrap_prior_dist(prior, transforms)
+        simulator = wrap_simulator_fn(simulator, transforms)
+
+    # DDM specific.
+    if isinstance(task, DDM):
+        observation = map_x_to_two_D(observation)
+
+    density_estimator_fun = likelihood_nn(
+        model=neural_net.lower(),
+        hidden_features=hidden_features,
+        z_score_x=z_score_x,
+        z_score_theta=z_score_theta,
+        **(mnle_kwargs or {}),
+    )
+    inference_method = inference.MNLE(
+        density_estimator=density_estimator_fun,
+        prior=prior,
+    )
+
+    posteriors = []
+    proposal = prior
+
+    for r in range(num_rounds):
+        theta, x = inference.simulate_for_sbi(
+            simulator,
+            proposal,
+            num_simulations=num_simulations_per_round,
+            simulation_batch_size=simulation_batch_size,
+        )
+        # Map to (rts, choices) for DDM.
+        if isinstance(task, DDM):
+            x = map_x_to_two_D(x)
+
+        density_estimator = inference_method.append_simulations(
+            theta, x, from_round=r
+        ).train(
+            training_batch_size=training_batch_size,
+            retrain_from_scratch=False,
+            discard_prior_samples=False,
+            show_train_summary=True,
+            max_num_epochs=max_num_epochs,
+        )
+        if r > 1:
+            mcmc_parameters["init_strategy"] = "latest_sample"
+
+        posterior = inference_method.build_posterior(
+            density_estimator=density_estimator,
+            sample_with="mcmc",
+            mcmc_method=mcmc_method,
+            mcmc_parameters=mcmc_parameters,
+        )
+        # Copy hyperparameters, e.g., mcmc_init_samples for "latest_sample" strategy.
+        if r > 0:
+            posterior.copy_hyperparameters_from(posteriors[-1])
+        proposal = posterior.set_default_x(observation)
+        posteriors.append(posterior)
+
+    posterior = wrap_posterior(posteriors[-1], transforms)
+
+    assert simulator.num_simulations == num_simulations
+
+    samples = posterior.sample((num_samples,)).detach()
+
+    return samples, simulator.num_simulations, None
